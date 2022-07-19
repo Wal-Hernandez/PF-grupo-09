a09727737e396ee021adb488fd6b974cad3969c0
ElGubb <gustybaig@hotmail.com>
2022-07-19 18:16:57 -0300

    Home actualizado

--- client/src/views/HomeView/index.jsx
+++ client/src/views/HomeView/index.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
 
 function HomeView() {
   return (
     <div>
     <div>
-      <h1>NAVBAR</h1>
+      <Navbar/>
     </div>
     <div>
       <h1>BODY</h1>
     </div>
     <div>
-      <h1>FOOTER</h1>
+      <Footer/>
     </div>
     </div>
   )

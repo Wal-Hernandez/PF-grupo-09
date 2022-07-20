9ca4b53639d1da51a2ff626a6c53dd88e1338fa5
mauroblanco7 <mauro.blnc.mb@gmail.com>
2022-07-20 14:02:43 -0300

    controlador y rutas get, post  de Hotel y City

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,16 @@
-const { Router } = require('express');
+const { Router } = require("express");
+
+const City = require("./citiesRouter");
+const Hotel = require("./hotelsRouter");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.use("/cities", City);
+router.use("/hotels", Hotel);
 
 module.exports = router;

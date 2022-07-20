ffd2f39cc30c695e6cb48978b924c1e481c94842
wal-hernandez <walter.3034@hotmail.com>
2022-07-20 15:35:26 -0300

    first interactive views

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './footer.css'
 
 function Footer() {
   return (
-    <div>
+    <div className='FooterContainer'>
       <h1>Acá van los elementos</h1>
     </div>
   )

d013ebdbb398102c514f338cebb6696278dddcde
DanoRiv <danio260496@gmail.com>
2022-07-20 12:46:25 -0500

    Package controller and get & post router

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,17 +1,18 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-const buses = require('./bus-router')
+const Buses = require('./busRouter')
 const Activity=require("./activityRouter")
 const Plattform=require("./plattformRouter")
+const Package = require('./packageRouter')
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.use('/buses', buses)
+router.use('/buses', Buses)
 router.use('/activities',Activity)
-
 router.use('/plattforms',Plattform)
+router.use('/packages', Package)
 
 module.exports = router;

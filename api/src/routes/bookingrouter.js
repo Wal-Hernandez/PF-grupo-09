efd9acbe6e1c39113f0bf7188b414392bd083718
Carlos <carloslamas3.0@gmail.com>
2022-07-20 13:27:26 -0300

    rutas Booking y unificacion del Booking con status

---
+++ api/src/routes/bookingrouter.js
@@ -0,0 +1,47 @@
+const {getAllBooking,getBookingByNumber,AddBooking,getDescriptionById} =require('../controllers/bookingcontroller.js')
+const {Router}=require("express")
+const router=Router()
+router.get('/booking', async(req,res,next)=>{
+
+    const{num} =req.query;
+    try{ if(!num){
+    let allBooking = await getAllBooking()
+    res.json(allBooking);
+    }
+    let numBooking = await getBookingByNumber(Number(num));
+    
+    if(numBooking){
+        console.log(numBooking)
+    res.json(numBooking)
+    }
+    
+    
+    }
+    catch(error){()=>{ next(error)}}
+    
+    })
+    router.get('/status', async(req,res,next)=>{
+
+        const{id} = req.query
+        try{ 
+            let numBooking = await getDescriptionById(Number(id));
+            
+            if(numBooking){
+                
+            res.json(numBooking)
+            }
+        }
+            catch(error){()=>{ next(error)}}
+    })
+    
+    router.post('/booking', async(req,res,next)=>{
+    const{dateTime, numberPeople, amount, description}= req.body
+    let a = new Date(dateTime);
+    console.log(a)
+    try{ let resp =await AddBooking(dateTime,numberPeople,amount,description)
+        res.json(resp)}
+    catch(error){next(error)}
+    
+    
+    })
+    module.exports=router;
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,11 +2,25 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+const Booking=require("./bookingrouter");
+const Status=require("./statusrouter");
+
+
+router.use('/',Booking);
+
+
+
+
+
+
+
+
+
 
 
 module.exports = router;

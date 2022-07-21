cc82c2c3f70758f3e1193d59ba5464e106a26be2
ElGubb <gustybaig@hotmail.com>
2022-07-21 17:06:15 -0300

    store arreglado

---
+++ client/src/components/Carousel/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Carousel() {
+  return (
+    <div>
+      <h3>Carrusel de ofertas</h3>
+    </div>
+  )
+}
+
+export default Carousel
--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,14 +1,12 @@
-import { createStore, applyMiddleware ,compose} from 'redux';
+import { legacy_createStore as createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from "../reducer";
 
 const store = createStore(
-	rootReducer,
-	compose(applyMiddleware(thunk))
+    rootReducer,
+    compose(applyMiddleware(thunk))
 );
 
 
 
- export default store;
-
-
+export default store;

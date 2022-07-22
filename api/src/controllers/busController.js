32d5e668f23d022808644af0cc92f2218204cf05
ArrayaAgustin <agustinarraya10@gmail.com>
2022-07-22 20:15:17 -0300

    commit

--- api/src/controllers/busController.js
+++ api/src/controllers/busController.js
@@ -18,14 +18,16 @@ const getBusesById = async (req, res, next) => {
   const { id } = req.params;
 
   try {
-    const busParam = id && await Bus.findAll({
-          where: {
-            id: {
-                [Op.eq]: `${Number(id)}`
-            }
+    const busParam =
+      id &&
+      (await Bus.findAll({
+        where: {
+          id: {
+            [Op.eq]: `${Number(id)}`,
           },
-        })
-    busParam.length   
+        },
+      }));
+    busParam.length
       ? res.status(200).json(busParam)
       : res.status(404).json({
           error: "Buses no encontrados",
@@ -38,37 +40,85 @@ const getBusesById = async (req, res, next) => {
   }
 };
 
-
-
 /* ----------------------------------------POST BUS------------------------------------------- */
 
-const postBus = async(req, res, next)=>{
-    const {patent, seating} = req.body;
+const postBus = async (req, res, next) => {
+  const { patent, seating } = req.body;
 
-    try {
-        await Bus.create({
-            patent, seating
-        })
-        res.status(201).send("Success")
-    } catch (error) {
-        res.json({
-          msg: "Couldn't create bus",
-          error: error
-        })
-    }
-}
+  try {
+    await Bus.create({
+      patent,
+      seating,
+    });
+    res.status(201).send("Success");
+  } catch (error) {
+    res.json({
+      msg: "Couldn't create bus",
+      error: error,
+    });
+  }
+};
 
 /* ----------------------------------------DELETE BUS------------------------------------------- */
 
-// const deleteBus = async(req, res, next)=>{
-//   try {
-//     const { id } = req.params;
-//     const busById = await findByPk(id);
-    
-    
-//   } catch (error) {
-    
-//   }
-// }
+const deleteBusesById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleteBuses = await Bus.destroy({
+      where: { id: id },
+    });
 
-module.exports = {getBuses, getBusesById, postBus };
+    if (deleteBuses) {
+      return res.status(201).json({
+        msg: "The bus has been removed successfully",
+        valor: true,
+      });
+    } else {
+      return res.status(400).json({
+        msg: "The bus cannot be removed because the id does not exist",
+      });
+    }
+  } catch (error) {
+    return res.status(400).json({
+      msg: "Error deleteBusesById(busController.js)",
+      error: error,
+    });
+  }
+};
+
+const updateBusById = async (req, res) => {
+  const { id } = req.params;
+  const { patent, seating } = req.body;
+  try {
+    if (!patent || !seating) {
+      return res.status(404).json({
+        msg: "All fields are required",
+      });
+    }
+    const a = await Bus.update({ patent, seating }, { where: { id: id } });
+    console.log(a);
+    if (a[0]) {
+      return res.status(201).json({
+        msg: "The bus has been update successfully",
+        valor: true,
+      });
+    } else {
+      return res.status(400).json({
+        msg: "The bus cannot be updated because the id does not exist",
+      });
+    }
+  } catch (error) {
+    return res.status(400).json({
+      msg: "Error updateBusById(busController.js)",
+      error: error,
+    });
+  }
+};
+
+module.exports = {
+  getBuses,
+  getBusesById,
+  postBus,
+  deleteBusesById,
+  updateBusById,
+};

9602284df187afcab820f58a5937dd0761b78850
ElGubb <gustybaig@hotmail.com>
2022-07-24 16:47:29 -0300

    localhost

--- client/src/redux/actions/getPackages.js
+++ client/src/redux/actions/getPackages.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 export const getPackages = () => {
     return async function(dispatch) {
         try {
-            let result = await axios.get(`http://localhost:3000/packages`);
+            let result = await axios.get(`http://localhost:3001/packages`);
             return dispatch({
                 type: GET_PACKAGES,
                 payload: result.data

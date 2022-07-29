833469adbdc4c6522dddf32416e721947a9c9093
DanoRiv <danio260496@gmail.com>
2022-07-29 10:56:47 -0500

    filtros y busqueda

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,12 +1,15 @@
-import { legacy_createStore as createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from "../reducer";
+import rootReducer from '../reducer';
+
+const composeEnhancers =
+   (typeof window !== 'undefined' &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+   compose;
 
 const store = createStore(
-    rootReducer,
-    compose(applyMiddleware(thunk))
+   rootReducer,
+   composeEnhancers(applyMiddleware(thunk)),
 );
-
-
 
 export default store;

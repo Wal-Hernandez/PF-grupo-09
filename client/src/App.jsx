aaf81e8865fc3fe2cc1b6237fd73d6645f460b99
mauroblanco7 <mauro.blnc.mb@gmail.com>
2022-07-28 21:06:04 -0300

    todo funciona bien perron

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -35,32 +35,68 @@ import { auth } from './Firebase/firebase-config';
 import {firestore} from "./context/context"
 
 function App() {
+  const [userlog, setUser] = React.useState(null);
 
+  async function getRol(uid) {
+    const docuRef = doc(firestore, `usuarios/${uid}`);
+    const docuCifrada = await getDoc(docuRef);
+    const infoFinal = docuCifrada.data().rol;
+    const infoFinal2 = docuCifrada.data().nombre;
+    const infoFinal3 = docuCifrada.data().apellido;
+    const infoFinal4 = docuCifrada.data().mail;
+    return [infoFinal,infoFinal2,infoFinal3,infoFinal4];
+  }
+
+  function setUserWithFirebaseAndRol(usuarioFirebase) {
+    getRol(usuarioFirebase.uid).then((rol) => {
+      const userData = {
+        uid: usuarioFirebase.uid,
+        email: usuarioFirebase.email,
+        rol: rol[0],
+        nombre:rol[1],
+        apellido: rol[2],
+        mail: rol[3]
+      };
+      setUser(userData);
+      console.log("userData fianl", userData);
+    });
+  }
+
+  onAuthStateChanged(auth, (usuarioFirebase) => {
+    if (usuarioFirebase) {
+      //funcion final
+
+      if (!userlog) {
+        setUserWithFirebaseAndRol(usuarioFirebase);
+      }
+    } else {
+      setUser(null);
+    }
+  });
 
   return (
     <div className="App">
      <AuthProvider>
      <Routes>
-       <Route path="/" element={<Home/>} />
-       {/* <Route path="/login" element={<LoginView/>}/> */}
+       <Route path="/" element={<Home userlog={userlog}/>} />
        <Route path="/details/:id" element={<Details/>} />
        <Route path="/buy" element={<EditBuy/>} />
        <Route path="/editBuy" element={<Buy/>} />
        <Route path="/services" element={<Services/>} />
-       <Route path="/admin" element={<ProtectedRoutedAdmin><Admin/></ProtectedRoutedAdmin>} />
+       <Route path="/admin" element={<ProtectedRoutedAdmin><Admin userlog={userlog} /></ProtectedRoutedAdmin>} />
        <Route path ="/faq" element ={<FAQ/>}/>
        <Route path="/about" element={<AboutView/>} />
-       <Route path="/admin/PutCityForm/:id" element ={<PutCityForm/>}/>
-       <Route path="/admin/create" element ={<CreateForm/>}/>
-       <Route path="/admin/edit/buses/:id" element ={<PutBusForm/>}/>
-       <Route path="/admin/edit/activities/:id" element ={<PutActivityForm/>}/>
-       <Route path="/admin/edit/packages/:id" element ={<PutPackageForm/>}/>
-       <Route path="/admin/edit/hotels/:id" element ={<PutHotelForm/>}/>
-       <Route path="/admin/edit/cities/:id" element ={<PutCityForm/>}/>
-       <Route path="/admin/edit/plattforms/:id" element ={<PutPlatformForm/>}/>
+       <Route path="/admin/PutCityForm/:id" element ={<ProtectedRoutedAdmin><PutCityForm/></ProtectedRoutedAdmin>}/>
+       <Route path="/admin/create" element ={<ProtectedRoutedAdmin><CreateForm/></ProtectedRoutedAdmin>}/>
+       <Route path="/admin/edit/buses/:id" element ={<ProtectedRoutedAdmin><PutBusForm/></ProtectedRoutedAdmin>}/>
+       <Route path="/admin/edit/activities/:id" element ={<ProtectedRoutedAdmin><PutActivityForm/></ProtectedRoutedAdmin>}/>
+       <Route path="/admin/edit/packages/:id" element ={<ProtectedRoutedAdmin><PutPackageForm/></ProtectedRoutedAdmin>}/>
+       <Route path="/admin/edit/hotels/:id" element ={<ProtectedRoutedAdmin><PutHotelForm/></ProtectedRoutedAdmin>}/>
+       <Route path="/admin/edit/cities/:id" element ={<ProtectedRoutedAdmin><PutCityForm/></ProtectedRoutedAdmin>}/>
+       <Route path="/admin/edit/plattforms/:id" element ={<ProtectedRoutedAdmin><PutPlatformForm/></ProtectedRoutedAdmin>}/>
        <Route path ="/reg" element ={<Register/>}/>
        <Route path ="/login" element ={<Login/>}/>
-       <Route path ="/home2" element ={<ProtectedRouted><Home2 /></ProtectedRouted>}/>
+      
      </Routes>
      </AuthProvider>
     

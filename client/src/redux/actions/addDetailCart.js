4c9cda5595e5fdf199639daf5f1570eb12038c3b
ArrayaAgustin <agustinarraya10@gmail.com>
2022-08-05 13:07:43 -0300

    correccion removeCart removeDetailCart addDetailCart
    se agrego el metodo clearCart en cart

--- client/src/redux/actions/addDetailCart.js
+++ client/src/redux/actions/addDetailCart.js
@@ -1,14 +1,16 @@
 import { ADD_DETAIL_CART } from "./actionTypes";
 import axios from "axios";
 
-export const addDetailCart = (idCart,id) => {
+export const addDetailCart = (idCart,id,email) => {
   return async function (dispatch) {
     try {
       
        let respuesta=await axios.post(`http://localhost:3001/cartdetails`,{idCart:idCart,idPackage:id,numberPeople:1,isQualified:false})
        console.log("RESPUESTA ADDDETAILCART:",respuesta.data)
+       let resp = await axios.get(`http://localhost:3001/carts/${email}`);
       return dispatch({
         type: ADD_DETAIL_CART,
+        payload:resp.data
       });
     } catch (err) {
       console.log(err);

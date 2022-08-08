f550c65a693e78b251d5864b56fc2867ac119867
Carlos <carloslamas3.0@gmail.com>
2022-08-08 19:57:47 -0300

    res

--- client/src/components/ProfileUser/ProfileUser.jsx
+++ client/src/components/ProfileUser/ProfileUser.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import Navbar from '../Navbar';
 import { useDispatch,useSelector } from 'react-redux';
+import { loadShopping } from '../../redux/actions/loadShopping';
+import './Profile.css'
 export default function ProfileUser ({userlog}){
-console.log(userlog)
-
+const dispatch = useDispatch()
+const {shopping}= useSelector(state=>state.rootReducer)
+React.useEffect(
+    ()=>{dispatch(loadShopping(userlog?.email))},[dispatch]
+)
+console.log(shopping)
 return(
 <div>
     <Navbar userlog={userlog}/>
@@ -14,12 +20,29 @@ return(
       <div >
         {/* <img src="img.jpg" alt="John" style={{ width: "100%" }} /> */}
         <h1 className='title'>{userlog?.nombre +" "+ userlog?.apellido}</h1>
-        <p>Historial de Compras</p>   
+        <p>Historial de Compras</p>
+        {shopping.length? shopping.map(data=><div className='card'>
+            <p className='title'>{data?.cartDetails[0].package?.name}</p>
+            <input type="checkbox"  id="spoiler2" /> 
+  <label for="spoiler2" >Actividades</label>
+<div class="spoiler">{data?.cartDetails[0].package?.activities?.map(a=><p className='title'>{a?.name}</p>)}
+    </div>
+    <input type="checkbox"  id="spoiler" /> 
+  <label for="spoiler" >Empresa</label>
+  <div class="spoiler">   <p className='title'>{data?.cartDetails[0].package?.business?.name}</p>
+  </div>
+  <input type="checkbox"  id="spoiler3" /> 
+  <label for="spoiler3" >Hospedaje</label>
+            
+  <div class="spoiler">  <p className='title'>{data?.cartDetails[0].package?.hotel?.name}</p>
+         </div> 
+            </div>)
+             :''}   
         <p> Opiniones y Puntajes</p>
         
         
         <p>
-          <button>Comentario</button>
+          <button className='coment-btn'>Comentario</button>
         </p>
      </div>
 

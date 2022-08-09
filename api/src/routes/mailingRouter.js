debdf267e60f3a6bcd6dabb2b8ee43827f1696d3
Carlos <carloslamas3.0@gmail.com>
2022-08-09 14:31:06 -0300

    r

--- api/src/routes/mailingRouter.js
+++ api/src/routes/mailingRouter.js
@@ -1,5 +1,5 @@
 const {Router}=require("express")
-const { enviarMail } = require("../controllers/mailingController")
+const { enviarMail, sendPaymentConfirmation } = require("../controllers/mailingController")
 
 const router=Router()
 router.post('/', async (req,res)=>{
@@ -17,6 +17,20 @@ catch(err){
 
 })
 
+router.post('/confirmation', async (req,res)=>{
+    try{ const {mail} =req.body /// no es por body
+    await sendPaymentConfirmation(mail)
+    res.status(201).json('Exitoo')
+    }
+    
+    catch(err){
+        return res.status(400).json(err);
+    }
+    
+    
+    
+    
+    })
 
 
 module.exports=router;

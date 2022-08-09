ba5402607cd238daffae34b783661ec6921045d7
DanoRiv <90010696+DanoRiv@users.noreply.github.com>
2022-08-09 11:08:55 -0500

    Update index.jsx

--- client/src/components/Filters/index.jsx
+++ client/src/components/Filters/index.jsx
@@ -1,53 +1,115 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { filterByDestiny } from '../../redux/actions/filterByDestiny';
-import { sortByPrice } from '../../redux/actions/sortByPrice';
-import { sortByStock } from '../../redux/actions/sortByStock';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { filterByActivity } from "../../../redux/actions/filterByActivity";
+import { sortByPrice } from "../../../redux/actions/sortByPrice";
+import { sortByStock } from "../../../redux/actions/sortByStock";
 
-function Filters({ setCurrentPage }) {
+function Filters(props) {
+  const [selectOrder, setSelectOrder] = useState("precio");
+  const dispatch = useDispatch();
 
-    const cities = useSelector(state => state.cities);
-    const dispatch = useDispatch();
-    const [priceOrder, setPriceOrder] = useState('');
-    const [stockOrder, setStockOrder] = useState('');
-
-    function handleCities(e) {
-        dispatch(filterByDestiny(e.target.value, priceOrder, stockOrder))
-        setCurrentPage(1)
+  function handleActivities(e) {
+    dispatch(filterByActivity(e.target.value, props.price, props.stock));
+    props.setActivity(e.target.value);
+    props.setCurrentPage(1);
+  }
+  function handleSortByPrice(e) {
+    let startShortDate = "";
+    if (props.startDate !== "") {
+      return (startShortDate = props.startDate?.toISOString());
     }
-    function handleSortByPrice(e) {
-        dispatch(sortByPrice(e.target.value))
-        setPriceOrder(e.target.value)
-        setCurrentPage(1)
-    }
-    function handleSortByStock(e) {
-        dispatch(sortByStock(e.target.value))
-        setStockOrder(e.target.value)
-        setCurrentPage(1)
+    dispatch(sortByPrice(e.target.value, props.city, startShortDate));
+    props.setPrice(e.target.value);
+    props.setCurrentPage(1);
+  }
+  function handleSortByStock(e) {
+    let startShortDate = "";
+    if (props.startDate !== "") {
+      return (startShortDate = props.startDate?.toISOString());
     }
 
+    dispatch(sortByStock(e.target.value, props.city, startShortDate));
+    props.setStock(e.target.value);
+    props.setCurrentPage(1);
+  }
 
-    return (
-        <div className='select-container'>
-            <select class="form-select" defaultValue="" onChange={handleSortByPrice}>
-                <option value="" disabled>Sort by price</option>
-                <option value="asc">Ascending</option>
-                <option value="desc">Descending</option>
-            </select>
-            <select class="form-select" defaultValue="" onChange={handleSortByStock}>
-                <option value="" disabled>Sort by stock</option>
-                <option value="asc">Ascending</option>
-                <option value="desc">Descending</option>
-            </select>
-            <select class="form-select" defaultValue="" onChange={handleCities}>
-                <option value="" disabled>Filter by destination city</option>
-                <option value="">Show all</option>
-                {cities.map((e, index) => (
-                    <option key={index}>{e.name}</option>
-                ))}
-            </select>
-        </div>
-    )
+  return (
+    <div className="select-container d-flex row-cols-3">
+      <div className="col-3 justify-content-center">
+        <form>
+          Ordenar por:
+          <div className="form-check">
+            <label>
+              <input
+                type="radio"
+                name="selectOrder"
+                value={"precio"}
+                onChange={(e) => {
+                  setSelectOrder(e.target.value);
+                }}
+                className="form-check-input"
+              />
+              Precio
+            </label>
+          </div>
+          <div className="form-check">
+            <label>
+              <input
+                type="radio"
+                name="selectOrder"
+                value={"stock"}
+                onChange={(e) => {
+                  setSelectOrder(e.target.value);
+                }}
+                className="form-check-input"
+              />
+              Stock
+            </label>
+          </div>
+        </form>
+      </div>
+      <div className="col-4 align-self-center">
+        {selectOrder === "precio" ? (
+          <select
+            className="form-select"
+            defaultValue=""
+            onChange={handleSortByPrice}
+            label={"Ordenar por precio"}
+          >
+            <option value="">Ordenar por precio</option>
+            <option label={"Menor precio"} value="asc"></option>
+            <option label={"Mayor precio"} value="desc"></option>
+          </select>
+        ) : selectOrder === "stock" ? (
+          <select
+            className="form-select"
+            defaultValue=""
+            onChange={handleSortByStock}
+            label={"Ordenar por stock"}
+          >
+            <option value="">Ordenar por stock</option>
+            <option label={"Menor stock"} value="asc"></option>
+            <option label={"Mayor stock"} value="desc"></option>
+          </select>
+        ) : null}
+      </div>
+      <div className="col-4 align-self-center">
+        <select
+          className="form-select"
+          defaultValue=""
+          onChange={handleActivities}
+        >
+          <option value="" disabled>
+            Filtrar por actividades
+          </option>
+          <option value="">Mostrar todo</option>
+          {props.activities?.map((e, index) => (
+            <option key={index}>{e.name}</option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
 }
 
-export default Filters
+export default Filters;
